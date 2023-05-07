e0a260251e05caa56dbd9bfda9abf36a04ea38e9
Abhimanyu Rana <abhimanyu.rana@postman.com>
2023-05-07 18:15:08 +0530

    setup

---
+++ main.js
@@ -0,0 +1,35 @@
+// import './style.css'
+async function loadIntoTable(url, table) {
+  const tableHead = table.querySelector("thead");
+  const tableBody = table.querySelector("tbody");
+  const response = await fetch(url);
+  const { headers, rows } = await response.json();
+
+  // Clear
+  tableHead.innerHTML = "<tr></tr>";
+  tableBody.innerHTML = "";
+
+  // Populate headers
+  for (const headerText of headers) {
+    const headerElement = document.createElement("th");
+
+    headerElement.textContent = headerText;
+    tableHead.querySelector("tr").appendChild(headerElement);
+  }
+
+  // Populate rows
+  for (const row of rows){
+    const rowElement = document.createElement("tr");
+
+    for (const cellText of row){
+      const cellElement = document.createElement("td");
+
+      cellElement.textContent = cellText;
+      rowElement.appendChild(cellElement);
+    }
+
+    tableBody.appendChild(rowElement);
+  }
+}
+
+loadIntoTable("./data.json", document.querySelector("table"));
